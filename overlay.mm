58cfd6aa240b8f1b47b6f77ea83ba6cdd8f947b9
keremvpn20-afk <keremvpn20@gmail.com>
2026-08-08 10:56:54 +0300

    Update overlay.mm

--- overlay.mm
+++ overlay.mm
@@ -3,8 +3,6 @@
 #include "sdk.hpp"
 #include "memory.hpp"
 #import <UIKit/UIKit.h>
-#include <thread>
-#include <chrono>
 
 @interface MainESPView : UIView
 @property (nonatomic, strong) CADisplayLink *displayLink;
@@ -18,7 +16,7 @@ static MainESPView *gEspView = nil;
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor clearColor];
-        self.userInteractionEnabled = NO;
+        self.userInteractionEnabled = NO; // Tıklamalar arkaya geçsin
         
         _displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(redraw)];
         [_displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
@@ -34,12 +32,14 @@ static MainESPView *gEspView = nil;
     }
 }
 
+// BİREBİR WURST CLIENT RENKLERİ
 static UIColor* ColorForContainer(int type) {
     switch (type) {
-        case 1: return [UIColor colorWithRed:0.94 green:0.62 blue:0.12 alpha:1.0]; // Chest: Turuncu
-        case 2: return [UIColor colorWithRed:0.12 green:0.82 blue:0.82 alpha:1.0]; // EnderChest: Camgöbeği
-        case 3: return [UIColor colorWithRed:0.55 green:0.55 blue:0.58 alpha:1.0]; // Hopper: Gri
-        case 4: return [UIColor colorWithRed:0.12 green:0.94 blue:0.12 alpha:1.0]; // Spawner: Yeşil
+        case 1: return [UIColor colorWithRed:1.0f green:0.53f blue:0.0f alpha:1.0]; // Normal Chest
+        case 2: return [UIColor colorWithRed:0.0f green:1.0f blue:0.53f alpha:1.0]; // Ender Chest
+        case 3: return [UIColor colorWithRed:0.6f green:0.6f blue:0.6f alpha:1.0];  // Hopper
+        case 4: return [UIColor colorWithRed:1.0f green:0.0f blue:0.0f alpha:1.0];  // Spawner
+        case 6: return [UIColor colorWithRed:0.55f green:0.27f blue:0.07f alpha:1.0]; // Barrel
         default: return [UIColor whiteColor];
     }
 }
@@ -50,23 +50,57 @@ static NSString* NameForContainer(int type) {
         case 2: return @"Ender Chest";
         case 3: return @"Hopper";
         case 4: return @"Spawner";
+        case 6: return @"Barrel";
         default: return @"Container";
     }
 }
 
+// WURST STYLE KUTU ÇİZİMİ (Gölgeli ve İçi Saydam Renkli)
+void DrawWurstBox(CGContextRef context, CGRect rect, UIColor* color) {
+    CGContextSetStrokeColorWithColor(context, [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.8].CGColor);
+    CGContextSetLineWidth(context, 3.0);
+    CGContextStrokeRect(context, rect);
+    
+    CGContextSetStrokeColorWithColor(context, color.CGColor);
+    CGContextSetLineWidth(context, 1.0);
+    CGContextStrokeRect(context, rect);
+    
+    CGContextSetFillColorWithColor(context, [color colorWithAlphaComponent:0.15].CGColor);
+    CGContextFillRect(context, rect);
+}
+
+// WURST STYLE TRACER (Siyah Gölge Üstü Canlı Çizgi)
+void DrawWurstTracer(CGContextRef context, CGPoint start, CGPoint end, UIColor* color) {
+    CGContextBeginPath(context);
+    CGContextMoveToPoint(context, start.x, start.y);
+    CGContextAddLineToPoint(context, end.x, end.y);
+    CGContextSetStrokeColorWithColor(context, [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.8].CGColor);
+    CGContextSetLineWidth(context, 2.5);
+    CGContextStrokePath(context);
+
+    CGContextBeginPath(context);
+    CGContextMoveToPoint(context, start.x, start.y);
+    CGContextAddLineToPoint(context, end.x, end.y);
+    CGContextSetStrokeColorWithColor(context, color.CGColor);
+    CGContextSetLineWidth(context, 1.0); 
+    CGContextStrokePath(context);
+}
+
 - (void)drawRect:(CGRect)rect {
+    if (!Hooks::storageEspEnabled) return;
+
     CGContextRef context = UIGraphicsGetCurrentContext();
     if (!context) return;
 
-    // ViewMatrix Kameramız
+    // TODO: Hâlâ 0x2A00000 kullanıyoruz. Ekranda kayma olursa ilk buradan şüpheleneceğiz.
     SDK::Matrix viewMatrix = *(SDK::Matrix*)(Memory::GetBaseAddress() + 0x2A00000);
 
     CGRect screenBounds = [UIScreen mainScreen].bounds;
     float width = screenBounds.size.width;
     float height = screenBounds.size.height;
     
-    // TRACER (Çizgi) Başlangıç Noktası (Ekranın en alt ortası)
-    CGPoint tracerStart = CGPointMake(width / 2.0f, height);
+    // Wurst Tracer genelde ekranın tam alt orta kısmından (crosshair'in çok altından) çıkar.
+    CGPoint tracerStartPos = CGPointMake(width / 2.0f, height); 
 
     std::vector<Hooks::MappedContainer> localList;
     {
@@ -77,13 +111,14 @@ static NSString* NameForContainer(int type) {
 
     int listYOffset = 150; 
     
-    // RADAR - Bulunan Sandık Sayısı
+    // WURST RADAR BİLGİSİ (Sol üst taraf)
     if (localList.size() > 0) {
-        NSString *title = [NSString stringWithFormat:@"FOUND CONTAINERS (%lu):", localList.size()];
+        NSString *title = [NSString stringWithFormat:@"[Wurst] Storage ESP: %lu", localList.size()];
         NSDictionary *attr = @{
             NSFontAttributeName: [UIFont systemFontOfSize:14.0 weight:UIFontWeightBlack],
-            NSForegroundColorAttributeName: [UIColor greenColor]
+            NSForegroundColorAttributeName: [UIColor whiteColor]
         };
+        [title drawAtPoint:CGPointMake(16, listYOffset + 1) withAttributes:@{NSFontAttributeName: [UIFont systemFontOfSize:14.0 weight:UIFontWeightBlack], NSForegroundColorAttributeName: [UIColor blackColor]}];
         [title drawAtPoint:CGPointMake(15, listYOffset) withAttributes:attr];
         listYOffset += 25;
     }
@@ -91,64 +126,24 @@ static NSString* NameForContainer(int type) {
     for (const auto& obj : localList) {
         UIColor *color = ColorForContainer(obj.type);
         
-        // RADAR - Sandık Koordinatları Listesi
-        NSString *listText = [NSString stringWithFormat:@"%@ -> X:%.0f  Y:%.0f  Z:%.0f", NameForContainer(obj.type), obj.worldPos.x, obj.worldPos.y, obj.worldPos.z];
-        
-        NSDictionary *shadowAttr = @{
-            NSFontAttributeName: [UIFont systemFontOfSize:13.0 weight:UIFontWeightBold],
-            NSForegroundColorAttributeName: [UIColor blackColor]
-        };
-        [listText drawAtPoint:CGPointMake(16, listYOffset + 1) withAttributes:shadowAttr];
-        
-        NSDictionary *textAttr = @{
-            NSFontAttributeName: [UIFont systemFontOfSize:13.0 weight:UIFontWeightBold],
-            NSForegroundColorAttributeName: color
-        };
-        [listText drawAtPoint:CGPointMake(15, listYOffset) withAttributes:textAttr];
-        
-        listYOffset += 20;
-
-        // 3D KUTU ve TRACER ÇİZİMİ
+        // EKRANDA 3D'Yİ 2D'YE ÇEVİR VE ÇİZ (WorldToScreen)
         SDK::Vector2 screen{};
         if (SDK::WorldToScreen(obj.worldPos, screen, viewMatrix, width, height)) {
             
-            NSString *labelName = [NSString stringWithFormat:@"%@", NameForContainer(obj.type)];
+            // Sandık Boyutu Kestirimi (Yaklaşık 30x30)
+            CGRect boxRect = CGRectMake(screen.x - 15, screen.y - 15, 30, 30);
+            DrawWurstBox(context, boxRect, color);
+
+            // İsim Yazısı
+            NSString *labelName = [NSString stringWithFormat:@"%@ [%.1fm]", NameForContainer(obj.type), obj.distance];
+            NSDictionary *shadowAttr = @{NSFontAttributeName: [UIFont systemFontOfSize:10.0 weight:UIFontWeightBold], NSForegroundColorAttributeName: [UIColor blackColor]};
+            NSDictionary *textAttr = @{NSFontAttributeName: [UIFont systemFontOfSize:10.0 weight:UIFontWeightBold], NSForegroundColorAttributeName: color};
             
-            // Kutu Çizimi
-            CGRect boxRect = CGRectMake(screen.x - 20, screen.y - 20, 40, 40);
-            
-            CGContextSetStrokeColorWithColor(context, [UIColor blackColor].CGColor);
-            CGContextSetLineWidth(context, 3.0);
-            CGContextStrokeRect(context, boxRect);
+            [labelName drawAtPoint:CGPointMake(screen.x - 20, screen.y - 30) withAttributes:shadowAttr]; 
+            [labelName drawAtPoint:CGPointMake(screen.x - 21, screen.y - 31) withAttributes:textAttr];   
 
-            CGContextSetStrokeColorWithColor(context, color.CGColor);
-            CGContextSetLineWidth(context, 1.5);
-            CGContextStrokeRect(context, boxRect);
-
-            // Kutunun üstüne İsmini Yazma
-            NSDictionary *attributes = @{
-                NSFontAttributeName: [UIFont systemFontOfSize:11.0 weight:UIFontWeightBold],
-                NSForegroundColorAttributeName: color
-            };
-            [labelName drawAtPoint:CGPointMake(screen.x - 20, screen.y - 35) withAttributes:attributes];
-
-            // TRACER (Lazer Çizgisi) Çizimi
             if (Hooks::drawTracers) {
-                // Önce Siyah Gölge Çizgi (Daha belirgin olması için)
-                CGContextBeginPath(context);
-                CGContextMoveToPoint(context, tracerStart.x, tracerStart.y);
-                CGContextAddLineToPoint(context, screen.x, screen.y);
-                CGContextSetStrokeColorWithColor(context, [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.7].CGColor);
-                CGContextSetLineWidth(context, 3.0);
-                CGContextStrokePath(context);
-
-                // Üstüne Kendi Renginde Çizgi
-                CGContextBeginPath(context);
-                CGContextMoveToPoint(context, tracerStart.x, tracerStart.y);
-                CGContextAddLineToPoint(context, screen.x, screen.y);
-                CGContextSetStrokeColorWithColor(context, color.CGColor);
-                CGContextSetLineWidth(context, 1.5);
-                CGContextStrokePath(context);
+                DrawWurstTracer(context, tracerStartPos, CGPointMake(screen.x, screen.y), color);
             }
         }
     }
@@ -159,7 +154,6 @@ namespace Overlay {
     void Initialize() {
         dispatch_async(dispatch_get_main_queue(), ^{
             gEspView = [[MainESPView alloc] initWithFrame:[UIScreen mainScreen].bounds];
-            
             id delegate = [UIApplication sharedApplication].delegate;
             if (delegate && [delegate respondsToSelector:@selector(window)]) {
                 UIWindow *win = [delegate performSelector:@selector(window)];
